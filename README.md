d160e1915c41dcc6fc04fcf23a2b7f19a21adc9b
skanjalkar <shreyaskanjalkar@gmail.com>
2023-12-06 12:16:06 -0500

    day6 part2 done

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ My attempt at Advent of Code 2023, using c++
 I have not tried to optimize the codes, but rather focus on the learning and perfection of my understanding of c++. Advent of code provides a good opportunity to solve unique problems and learn about string parsing, data conversions, some simple algorithms and data structures.
 
 I am also using github actions to ensure all of my pushes do actually compile. Just a simple CI addition :D 
+
+The compiler I have used is g++, in order to run the code, navigate to the respective day and 
+```g++ part1.cpp -o part1.o```
+
+And in order to run the code, 
+
+```./part1.o <input.txt``` 
--- day6/part2.cpp
+++ day6/part2.cpp
@@ -1,9 +1,65 @@
 #include<iostream>
 #include<stdlib.h>
+#include<vector>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+#include<sstream>
+#include<limits.h>
 
 using namespace std;
 
 int main() {
+    string time;
+    string distance;
+
+    getline(cin, time);
+    getline(cin, distance);
+
+    time = time.substr(5);
+    distance = distance.substr(9);
+
+    time.erase(remove_if(time.begin(), time.end(), ::isspace), time.end());
+    distance.erase(remove_if(distance.begin(), distance.end(), ::isspace), distance.end());
+
+    long long iTime = stoll(time);
+    long long iDistance = stoll(distance);
+
+    long long low = 1;
+    long long high = iTime;
+    long long temp;
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long currDistance = mid * (iTime - mid);
+
+        if (currDistance > iDistance) {
+            temp = mid;
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+
+    long long low2 = 1;
+    long long high2 = iTime;
+    long long temp2;
+    while (low2 <= high2) {
+        long long mid = (low2 + high2) / 2;
+        long long currDistance = mid * (iTime - mid);
+
+        if (currDistance > iDistance) {
+            temp2 = mid;
+            low2 = mid + 1;
+        }
+        else {
+            high2 = mid - 1;
+        }
+    }
+    // idist - (idist - temp2) - temp1
+    //
+    cout << temp2 - temp + 1 << endl;
 
     return 0;
 }
