a672eb1a5971a0cf7466f61b54d0ee0497b1467e
skanjalkar <shreyaskanjalkar@gmail.com>
2023-12-15 13:52:18 -0500

    day15 p1 sample done

---
+++ day15/part1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<stdlib.h>
+#include<vector>
+#include<string>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+#include<sstream>
+#include<limits.h>
+
+using namespace std;
+
+void solve(vector<string>& seqeunces) {
+    int ans = 0;
+    for (string & s : seqeunces) {
+        int temp = 0;
+        for (char c : s) {
+            temp += int(c);
+            temp = temp * 17;
+            temp = temp % 256;
+        }
+        cout << temp << endl;
+        ans += temp;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+    vector<string> sequences;
+    // split string by all , and store in sequences
+    stringstream ss(input);
+    string token;
+    while(getline(ss, token, ',')) {
+        sequences.push_back(token);
+    }
+
+    solve(sequences);
+
+    return 0;
+}
